20b2a0051a13dbe9f731b9282451cfe550aa5e39
jordennorton <jordennorton@hotmail.co.uk>
2024-03-16 18:56:08 +0000

    Create About component

---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="about">
+        <section class="main-content">
+            <p>Transforming Your Outdoor Spaces</p>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AboutComponent",
+    props: {
+
+    }
+};
+</script>
+
+<style scoped>
+
+.main-content {
+    padding: 20px;
+    text-align: center;
+}
+
+</style>
