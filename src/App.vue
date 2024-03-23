337cb484ced962290d6d85eb2af826a7fed7ba6f
Jorden Norton <jordennorton@hotmail.co.uk>
2024-03-23 22:01:56 +0000

    Add home page images

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,42 @@
             :backgroundImage="backgroundImage"
         />
         <!-- Main Content Section -->
-        <AboutComponent />
+        <AboutComponent/>
+        <PortfolioComponent
+            :portfolioOne="portfolioOne"
+            :portfolioTwo="portfolioTwo"
+            :portfolioThree="portfolioThree"
+            :portfolioFour="portfolioFour"
+        />
     </div>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import AboutComponent from "@/components/AboutComponent.vue";
+import PortfolioComponent from "@/components/PortfolioComponent.vue";
 import logoImage from '@/assets/logo.png'; // Adjust this path to where your actual logo image is located
 import bgImage from '@/assets/bg.jpg'; // Adjust this path to where your actual background image is located
+import portfolioOne from '@/assets/portfolio/image1.jpeg';
+import portfolioTwo from '@/assets/portfolio/image2.jpeg';
+import portfolioThree from '@/assets/portfolio/image3.jpeg';
+import portfolioFour from '@/assets/portfolio/image4.jpeg';
 
 export default {
     name: 'App',
     components: {
         HeaderComponent,
-        AboutComponent
+        AboutComponent,
+        PortfolioComponent
     },
     data() {
         return {
             logo: logoImage,
             backgroundImage: bgImage,
+            portfolioOne: portfolioOne,
+            portfolioTwo: portfolioTwo,
+            portfolioThree: portfolioThree,
+            portfolioFour: portfolioFour,
         };
     },
 }
